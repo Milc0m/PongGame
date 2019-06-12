86e4f6bc0523c23fa5611f44f1e2c1119b652e7c
VolodymyrHav <dem82003@protonmail.com>
2019-06-12 19:23:07 +0300

    Create landing page

---
+++ PongGame/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PongGame.Models
+{
+    public class LoginModel
+    {
+        public string Name { get; set; }
+    }
+}
